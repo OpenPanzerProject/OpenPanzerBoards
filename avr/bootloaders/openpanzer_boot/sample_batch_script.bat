a008cbe89fafb6dd02e53618cfc28d54acc796a0
Open Panzer <admin@openpanzer.org>
2019-07-31 21:52:53 -0500

    Update board files

--- avr/bootloaders/openpanzer_boot/sample_batch_script.bat
+++ avr/bootloaders/openpanzer_boot/sample_batch_script.bat
@@ -3,10 +3,46 @@ REM - This will burn a bootloader. First it will set the fuse bits and unlock th
 REM - will burn the bootloader and re-lock the bootloader section
 REM ----------------------------------------------------------------------------------------------------------->>
 
-REM - Do NOT use white space between the variable name, the equal sign, and the value! 
+
+REM ----------------------------------------------------------------------------------------------------------->>
+REM ----------------------------------------------------------------------------------------------------------->>
+REM -   SET YOUR PATHS HERE 
+REM - 
+REM -   CHANGE THESE PATHS TO MATCH YOUR LOCAL SYSTEM
+REM -   AVOID SPACES AND OTHER SPECIAL CHARACTERS IN YOUR PATH ! 
+REM - 
+REM ----------------------------------------------------------------------------------------------------------->>
+REM - Not having any spaces in the file name will save you infinite heartache. 
+REM - You also for sure don't want any parentheses in the file name either, AVRDUDE will croak for sure if it sees those
+
+REM - Path to avrdude.conf file: 
+set conf_file=C:\Arduino\hardware\tools\avr\etc\avrdude.conf
+
+REM - Path to avrdude.exe
+set avrdude_exe=C:\Arduino\hardware\tools\avr\bin\avrdude.exe
+
+REM - Path to bootloader hex
+set boot_hex=C:\TCBBootloader\optcb2560_boot.hex
 
 
-REM - FUSES - DON'T CHANGE THESE, THEY ARE CORRECT FOR THE OPEN PANZER TCB Mk 1
+
+REM ----------------------------------------------------------------------------------------------------------->>
+REM ----------------------------------------------------------------------------------------------------------->>
+REM -   SET YOUR PROGRAMMER TYPE HERE 
+REM - 
+REM ----------------------------------------------------------------------------------------------------------->>
+REM - See http://www.nongnu.org/avrdude/user-manual/avrdude_4.html for a list of programmers, or type avrdude -c ?
+REM - Use this for the USBasp. If using the USBasp, you can also ignore the warning message avrdude will give you about sck 
+REM - The two most common aare usbasp and avrispmkII
+
+REM set programmer=avrispmkII
+set programmer=usbasp           
+
+
+REM ----------------------------------------------------------------------------------------------------------->>
+
+
+REM - FUSES - Don't change these, they are correct for the TCB Mk !
 REM ----------------------------------------------------------------------------------------------------------->>
 set EFUSE=0xFD
 set HFUSE=0xDA
@@ -16,35 +52,9 @@ set LOCKF=0x0F
 
 
 
-REM - SET YOUR AVRDUDE AND CONF FILE PATHS
-REM ----------------------------------------------------------------------------------------------------------->>
-REM - Notice I've moved Arduino to the root of C:. Not having any spaces in the file name will save you infinite heartache. 
-REM - You also for sure don't want any parentheses in the file name either, AVRDUDE will croak for sure if it sees those
-
-set conf_file="C:\YOUR_ARDUINO_INSTALL_DIR\hardware\tools\avr\etc\avrdude.conf"
-
-set avrdude_exe=C:\YOUR_ARDUINO_INSTALL_DIR\hardware\tools\avr\bin\avrdude.exe
-
-
-
-REM - SET YOUR BOOTLOADER HEX PATH
-REM ----------------------------------------------------------------------------------------------------------->>
-set boot_hex="C:\WHEREVER_YOU_PUT_IT\openpanzer_boot\optcb2560_boot.hex"
-
-
-
-REM - SET YOUR PROGRAMMER
-REM ----------------------------------------------------------------------------------------------------------->>
-REM - See http://www.nongnu.org/avrdude/user-manual/avrdude_4.html for a list of programmers, or type avrdude -c ?
-REM - Use this for the USBasp. If using the USBasp, you can also ignore the warning message avrdude will give you about sck 
-set programmer=usbasp           
-
-
-
 REM - THIS WILL DO THE EXECUTING - DON'T CHANGE ANYTHING BELOW HERE
 REM ----------------------------------------------------------------------------------------------------------->>
 REM - The start cmd.exe /k lets us open the command prompt first and then leave it open so we can see if it worked. 
 REM - First half: chip erase (-e), unlock boot section, write fuses. Next comes a "+" symbol to add the second half: 
 REM - Second half: now we write the bootloader to flash, then finish by locking the boot section
-start cmd.exe /k %avrdude_exe% -C %conf_file% -p atmega2560 -c %programmer% -P usb -v -e -U lock:w:%ULOCKF%:m -U efuse:w:%EFUSE%:m -U hfuse:w:%HFUSE%:m -U lfuse:w:%LFUSE%:m + %avrdude_exe% -C %conf_file% -p atmega2560 -c %programmer% -P usb -v -U flash:w:%boot_hex%:i -U lock:w:%LOCKF%:m 
-
+start "Flash Bootloader" cmd.exe /K "%avrdude_exe% -C %conf_file% -p atmega2560 -c %programmer% -P usb -v -e -U lock:w:%ULOCKF%:m -U efuse:w:%EFUSE%:m -U hfuse:w:%HFUSE%:m -U lfuse:w:%LFUSE%:m && %avrdude_exe% -C %conf_file% -p atmega2560 -c %programmer% -P usb -v -U flash:w:%boot_hex%:i -U lock:w:%LOCKF%:m" 
